e1a3d87b3bd5c08865441cf3e2f4bf9f4218aa71
Fernando Catacora <nando.drw@gmail.com>
2016-09-02 12:35:54 -0500

    test for sha change

--- dist/reactTemplates_es5.js
+++ dist/reactTemplates_es5.js
@@ -2,6 +2,7 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
+
 var cheerio = require('cheerio');
 var _ = require('lodash');
 var esprima = require('esprima');
